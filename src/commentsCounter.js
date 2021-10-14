a776dabd01c47b1f2f770f5e88fe281c5e231877
SunnySparks <Josediazt6@outlook.com>
2021-10-14 14:20:26 -0500

    Fix linters errors in testing

--- src/commentsCounter.js
+++ src/commentsCounter.js
@@ -1,5 +1,7 @@
 export default function commentsCounter(comments) {
-    let length = 0;
-    comments.forEach(comment => length = length+1);
+  let length = 0;
+  comments.forEach(() => {
+    length += 1;
+  });
   return length;
-};
+}
